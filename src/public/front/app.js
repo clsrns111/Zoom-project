631efe8121c940510996bafc6b022a9f71b8b9ca
clsrns111 <clsrns1111@gmail.com>
2021-08-17 21:58:46 +0900

    webSocket 실시간채팅

--- src/public/front/app.js
+++ src/public/front/app.js
@@ -1,8 +1,55 @@
-const webSocket = new WebSocket("ws://localhost:3000");
-webSocket.onopen = function () {
+const ul = document.querySelector("ul");
+const message = document.getElementById("message");
+const nickname = document.getElementById("nickname");
+const webSocket = new WebSocket(`ws://${window.location.host}`);
+/* 1.webSocket.onopen = function () {
   console.log("서버와 웹 소켓 연결됨");
-};
-webSocket.onmessage = function (event) {
+}; */
+//2
+
+webSocket.addEventListener("open", () => {
+  console.log("서버와 웹 소켓 연결됨!");
+});
+
+/*1 webSocket.onmessage = function (event) {
   console.log(event.data);
   webSocket.send("클라이언트에서 서버로 답장을 보냅니다.");
-};
+}; */
+//2
+webSocket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerHTML = `${message.data}`;
+  ul.appendChild(li);
+});
+
+/* webSocket.addEventListener("close", () => {
+  console.log("서버와 웹 소켓 끊김");
+}); */
+/* 
+setTimeout(() => {
+  webSocket.send("나는 클라이언트다");
+}, 2000); */
+
+function makeJson(type, data) {
+  return JSON.stringify({
+    type,
+    data,
+  });
+}
+
+message.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const input = message.querySelector("input");
+  webSocket.send(makeJson("message", input.value));
+  const li = document.createElement("li");
+  li.innerHTML = `YOU: ${input.value}`;
+  ul.append(li);
+  input.value = "";
+});
+
+nickname.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const input = nickname.querySelector("input");
+  webSocket.send(makeJson("nickname", input.value));
+  input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+const http = require("http");
 const express = require("express");
 const pug = require("pug");
 const path = require("path");
 const app = express();
+const WebSocket = require("ws");
 const port = 3000;
 const indexRouter = require("./routes/index");
 const webSocket = require("./routes/soket");
@@ -9,9 +11,33 @@ const webSocket = require("./routes/soket");
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 app.use("/public", express.static(__dirname + "/public"));
-app.use("/", indexRouter);
-const server = app.listen(port, () => {
-  console.log(port, "번 포트에서 대기중");
+app.get("/", (_, res) => res.render("home"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const sokets = [];
+
+wss.on("connection", (soket) => {
+  sokets.push(soket);
+  soket.onmessage = (message) => {
+    const messageobj = JSON.parse(message.data);
+    switch (messageobj.type) {
+      case "message":
+        return sokets.forEach((el) => {
+          el.send(`${soket.nickname}:${messageobj.data}`);
+        });
+      case "nickname":
+        return (soket["nickname"] = messageobj.data);
+      default:
+        break;
+    }
+  };
+
+  soket.on("close", () => {
+    console.log("클라이언트로부터 연결이 끊겼습니다.");
+  });
 });
 
-webSocket(server);
+server.listen(port, () => {
+  console.log("connected loaclhost:3000");
+});
